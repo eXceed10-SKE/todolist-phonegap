6adbdef743e6bba8c9937e6bb57fb13a22d54252
Kanin Sirisith <Arm@Kanins-MacBook-Pro.local>
2013-05-04 17:18:29 +0700

    edit main.js

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -12,3 +12,39 @@ var app = {
         console.log('asdas')
     }
 };
+
+var TODOList = []
+
+
+$(document).ready(function(){
+
+	$(".act").on("click",Act());
+
+	$(".com").on("click",Complete());
+
+	$(".alls").on("click",All());
+
+	$(".add").on("click",Add());
+
+});
+
+function Act()
+{
+	
+}
+
+function Complete()
+{
+
+}
+
+function All()
+{
+
+}
+
+function Add()
+{
+	var todo = {name:$(".add").text(), state:"Active"};
+	TODOList.push(todo);
+}
